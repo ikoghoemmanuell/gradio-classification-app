b344d14b010c6134bdafa2dd1e74c51b6e3d4266
ikoghoemmanuell <oke.kogo@gmail.com>
2023-04-11 07:06:14 +0100

    now the model is the problem

--- churn.py
+++ churn.py
@@ -12,75 +12,53 @@ scaler = pickle.load(open("scaler.pkl", "rb"))
 data = pd.read_csv('data.csv')
 
 # Define the input and output interfaces for the Gradio app
-input_components = [
-    gr.Dropdown(choices=list(data['gender'].unique()), label="Gender"),
-    gr.Radio(choices=list(data['Partner'].unique()), label="Partner"),
-    gr.Radio(choices=list(data['Dependents'].unique()), label="Dependents"),
-    gr.Radio(choices=['None', 'SingleLine', 'MultipleLines'], label="Phone Service"),
-    gr.Radio(choices=["DSL", "Fiber optic", "No"], label="Internet Service"),
-    gr.Radio(choices=["Yes", "No"], label="Online Backup"),
-    gr.Radio(choices=["Yes", "No"], label="Tech Support"),
-    gr.Dropdown(choices=["Month-to-month", "One year", "Two year"], label="Contract"),
-    gr.Radio(choices=list(data['PaperlessBilling'].unique()), label="Paperless Billing"),
-    gr.Dropdown(choices=["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"], label="Payment Method"),
-    gr.Radio(choices=['FullService', 'TV', 'Movies'], label="Streaming Service"),
-    gr.Radio(choices=['FullSecurity', 'OnlineSecurity', 'DeviceProtection'], label="Security Service"),
-    gr.Radio(choices=list(data['SeniorCitizen'].unique()), label="Senior Citizen"),
-    gr.Slider(minimum=1, maximum=data['tenure'].max(), step=1, label="Tenure (Month)"),
-    gr.Slider(maximum=data['MonthlyCharges'].max(), step=0.5, label="Monthly Charges"),
-    gr.Slider(maximum=data['TotalCharges'].max(), step=0.5, label="Total Charges")
-]
+def create_gradio_inputs(data):
+    input_components = []
+    for column in data.columns:
+        if data[column].dtype == 'object' and len(data[column].unique()) > 3:
+            input_components.append(gr.Dropdown(choices=list(data[column].unique()), label=column))
+        elif data[column].dtype == 'object' and len(data[column].unique()) <= 3:
+            input_components.append(gr.Radio(choices=list(data[column].unique()), label=column))
+        elif data[column].dtype in ['int64', 'float64']:
+            if data[column].min() == 1:
+                input_components.append(gr.Slider(minimum=1, maximum=data[column].max(), step=1, label=column))
+            else:
+                input_components.append(gr.Slider(maximum=data[column].max(), step=0.5, label=column))
+    return input_components
+
+input_components = create_gradio_inputs(data)
 
 output_components = [
     gr.Label(label="Churn Prediction"),
 ]
 
 # Convert the input values to a pandas DataFrame with the appropriate column names
-def input_df_creator(gender, partner, dependents, phone_service, internet_service, online_backup, tech_support, 
-                     contract, paperless_billing, payment_method, streaming_service, security_service, senior_citizen, 
-                     tenure, monthly_charges, total_charges):
-    input_data = pd.DataFrame({
-        "gender": [gender],
-        "Partner": [partner],
-        "Dependents": [dependents],
-        "PhoneService": [phone_service],
-        "InternetService": [internet_service],
-        "OnlineBackup": [online_backup],
-        "TechSupport": [tech_support],
-        "Contract": [contract],
-        "PaperlessBilling": [paperless_billing],
-        "PaymentMethod": [payment_method],
-        "StreamingService": [streaming_service],
-        "SecurityService": [security_service],
-        "SeniorCitizen": [int(senior_citizen)],
-        "tenure": [int(tenure)],
-        "MonthlyCharges": [float(monthly_charges)],
-        "TotalCharges": [float(total_charges)],
-    }) 
+def input_df_creator():
+    input_labels = [input.label for input in input_components]
+    input_values = [input.value for input in input_components]
+    input_data = pd.DataFrame([input_values], columns=input_labels)
     return input_data
 
-
-
 # Define the function to be called when the Gradio app is run
-def predict_churn(gender, partner, dependents, phone_service, internet_service, online_backup, tech_support, 
-                     contract, paperless_billing, payment_method, streaming_service, security_service, senior_citizen, 
-                     tenure, monthly_charges, total_charges):
+def predict_churn(*inputs):
     input_df = input_df_creator()
     # Encode categorical variables
     cat_cols = input_df.select_dtypes(include=['object']).columns
-    print(input_df[cat_cols])
-    print(len(input_df[cat_cols].columns))
-    print(len(input_df[cat_cols].keys()))
-    print(len(input_df[cat_cols].values))
-    input_df[cat_cols] = encoder.transform(input_df[cat_cols].fillna('NaN'))
+    cats = encoder.fit_transform(input_df[cat_cols].fillna('NaN'))
 
     # Scale numerical variables
     num_cols = input_df.select_dtypes(include=['int64', 'float64']).columns
-    input_df[num_cols] = scaler.transform(input_df[num_cols])
+    nums = scaler.transform(input_df[num_cols])
+
+    # joining encoded and scaled columns back together
+    processed_df = pd.concat([cats, nums], axis=1)
+    print(input_df[cat_cols])
+    print(processed_df.columns)
+
     # Make prediction
-    prediction = model.predict(input_df)
+    prediction = model.predict(processed_df)
     return "Churn" if prediction[0] == 1 else "No Churn"
 
 # Launch the Gradio app
 iface = gr.Interface(predict_churn, inputs=input_components, outputs=output_components)
-iface.launch()
+iface.launch(inbrowser= True, show_error= True)
